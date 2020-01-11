18c814f29ca289485156870c68058f4b748e9636
Yingyong Cao <yingycao@thoughtworks.com>
2020-01-11 11:19:44 +0800

    Refactor tests by using parameterized tests

--- fizz-buzz/src/test/java/com/code/practice/fizz/buzz/FizzBuzzTest.java
+++ fizz-buzz/src/test/java/com/code/practice/fizz/buzz/FizzBuzzTest.java
@@ -1,28 +1,32 @@
 package com.code.practice.fizz.buzz;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
+
+  private static Stream<Arguments> produceSpecialNumbers() {
+    return Stream.of(
+        Arguments.of("Fizz", 3),
+        Arguments.of("Buzz", 5),
+        Arguments.of("Whizz", 7));
+  }
 
   @Test
   void should_return_number() {
     assertEquals("1", FizzBuzz.fizzBuzz(1));
   }
 
-  @Test
-  void should_return_fizz_when_divisible_by_3() {
-    assertEquals("Fizz", FizzBuzz.fizzBuzz(3));
+  @ParameterizedTest
+  @MethodSource("produceSpecialNumbers")
+  void should_return_special_string_when_divisible_special_number(String expected, Integer number) {
+    assertEquals(expected, FizzBuzz.fizzBuzz(number));
   }
 
-  @Test
-  void should_return_buzz_when_divisible_by_5() {
-    assertEquals("Buzz", FizzBuzz.fizzBuzz(5));
-  }
-
-  @Test
-  void should_return_whizz_when_divisible_by_7() {
-    assertEquals("Whizz", FizzBuzz.fizzBuzz(7));
-  }
 }
