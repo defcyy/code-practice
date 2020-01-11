614e1c7e6d7084ce11d0407b16b110d264d809d4
Yingyong Cao <yingycao@thoughtworks.com>
2020-01-11 21:47:32 +0800

    Using rules refacter conditions

--- fizz-buzz/src/main/java/com/code/practice/fizz/buzz/FizzBuzz.java
+++ fizz-buzz/src/main/java/com/code/practice/fizz/buzz/FizzBuzz.java
@@ -1,5 +1,12 @@
 package com.code.practice.fizz.buzz;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class FizzBuzz {
   private static String FIZZ = "Fizz";
   private static String BUZZ = "Buzz";
@@ -7,35 +14,68 @@ public class FizzBuzz {
 
   public static String fizzBuzz(Integer number) {
 
+    List<Rule> rules = Arrays.asList(
+        new Rule(
+            num -> FIZZ,
+            num -> !containsNumber(num, "5") && containsNumber(num, "3")
+            , true),
+        new Rule(
+            num -> FIZZ,
+            num -> (containsNumber(num, "7") || !containsNumber(num, "5")) && num % 3 == 0,
+            false
+        ),
+        new Rule(
+            num -> BUZZ,
+            num -> !containsNumber(num, "7") && num % 5 == 0,
+            false
+        ),
+        new Rule(
+            num -> WHIZZ,
+            num -> number % 7 == 0,
+            false
+        )
+    );
 
-    if (!containsNumber(number, "5") && containsNumber(number, "3")) {
-      return FIZZ;
+    Optional<Rule> terminationRule = rules.stream()
+        .filter(r -> r.test(number))
+        .filter(Rule::isTermination)
+        .findFirst();
+    if (terminationRule.isPresent()) {
+      return terminationRule.get().process(number);
     }
 
-    String divisibleResult = processDivisible(number);
-
-    if (divisibleResult.isEmpty()) {
-      return number.toString();
-    }
-    return divisibleResult;
+    String matched = rules.stream()
+        .filter(r -> r.test(number))
+        .map(r -> r.process(number))
+        .collect(Collectors.joining());
+    return matched.isEmpty() ? number.toString() : matched;
   }
 
   private static boolean containsNumber(Integer number, String character) {
     return number.toString().contains(character);
   }
 
-  private static String processDivisible(Integer number) {
-    StringBuilder specialBuilder = new StringBuilder();
-    if ((containsNumber(number, "7") || !containsNumber(number, "5")) && number % 3 == 0) {
-      specialBuilder.append(FIZZ);
-    }
-    if (!containsNumber(number, "7") && number % 5 == 0) {
-      specialBuilder.append(BUZZ);
+  private static class Rule {
+    private boolean isTermination;
+    private Function<Integer, String> func;
+    private Predicate<Integer> predicate;
+
+    public Rule(Function<Integer, String> func, Predicate<Integer> predicate, boolean isTermination) {
+      this.func = func;
+      this.isTermination = isTermination;
+      this.predicate = predicate;
     }
 
-    if (number % 7 == 0) {
-      specialBuilder.append(WHIZZ);
+    public boolean isTermination() {
+      return isTermination;
     }
-    return specialBuilder.toString();
+
+    public boolean test(Integer number) {
+      return predicate.test(number);
+    }
+
+    public String process(Integer number) {
+      return func.apply(number);
+    }
   }
 }
