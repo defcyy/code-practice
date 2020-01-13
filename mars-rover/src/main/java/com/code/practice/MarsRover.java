575a634229cdddfe5483f21ea5ad6f97f71164bc
Yingyong Cao <yingycao@thoughtworks.com>
2020-01-14 00:00:37 +0800

    Add mars rover commands handler

--- mars-rover/src/main/java/com/code/practice/MarsRover.java
+++ mars-rover/src/main/java/com/code/practice/MarsRover.java
@@ -3,18 +3,22 @@ package com.code.practice;
 public class MarsRover {
   private State state;
 
-  public MarsRover() {
-
-  }
-
   private MarsRover(State state) {
     this.state = state;
   }
 
-  public String report() {
-    return state.toString();
+  public static MarsRover initialize(InitializeCommand command) {
+    return new MarsRover(new State(command));
   }
 
-  public void process(InitializeCommand command) {
+  public String process(String commands) {
+    commands.chars().forEach(c ->
+        state.execute(new ExecuteCommand(ExecuteCommand.Action.valueOf(String.valueOf((char) c)))));
+    return this.toString();
+  }
+
+  @Override
+  public String toString() {
+    return state.toString();
   }
 }
--- mars-rover/src/test/java/com/code/practice/MarsRoverTest.java
+++ mars-rover/src/test/java/com/code/practice/MarsRoverTest.java
@@ -7,10 +7,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MarsRoverTest {
 
   @Test
-  void should_return_information_when_receive_initial_command() {
+  void should_init_rover_with_initial_command() {
     InitializeCommand command = new InitializeCommand(0, 0, "W");
-    MarsRover marsRover = new MarsRover();
-    marsRover.process(command);
-    assertEquals(marsRover.report(), "0 0 W");
+    MarsRover marsRover = MarsRover.initialize(command);
+    assertEquals("0 0 W", marsRover.toString());
+  }
+
+  @Test
+  void should_process_commands() {
+    InitializeCommand command = new InitializeCommand(0, 0, "N");
+    MarsRover marsRover = MarsRover.initialize(command);
+    assertEquals("2 1 N", marsRover.process("MRMML"));
   }
 }
